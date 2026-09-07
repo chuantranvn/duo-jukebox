4ff2724b38eb5450059636a80f6fc460e30383e9
chuan.tran <adjim.loanthe@gmail.com>
2026-09-07 07:11:14 +0700

    Add push_to_git.bat script

---
+++ push_to_git.bat
@@ -0,0 +1,8 @@
+@echo off
+cd /d "%~dp0"
+echo ===================================================
+echo   DANG DAY TOAN BO SOURCE CODE LEN GITHUB...
+echo ===================================================
+git push -u origin main
+echo ===================================================
+pause
